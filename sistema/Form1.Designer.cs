1081bec0adf0e96a1cef6522fa2fd595e6efce47
Valentina <valenpinzonn@gmail.com>
2023-04-18 20:58:37 -0500

    login basico

---
+++ sistema/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace sistema
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textUsuario = new System.Windows.Forms.TextBox();
+            this.textContrasena = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textUsuario
+            // 
+            this.textUsuario.Location = new System.Drawing.Point(171, 65);
+            this.textUsuario.Name = "textUsuario";
+            this.textUsuario.Size = new System.Drawing.Size(392, 20);
+            this.textUsuario.TabIndex = 0;
+            this.textUsuario.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textContrasena
+            // 
+            this.textContrasena.Location = new System.Drawing.Point(171, 146);
+            this.textContrasena.Name = "textContrasena";
+            this.textContrasena.PasswordChar = '*';
+            this.textContrasena.Size = new System.Drawing.Size(392, 20);
+            this.textContrasena.TabIndex = 1;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(320, 216);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.textContrasena);
+            this.Controls.Add(this.textUsuario);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textUsuario;
+        private System.Windows.Forms.TextBox textContrasena;
+        private System.Windows.Forms.Button btnLogin;
+    }
+}
+
---
+++ sistema/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sistema
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        SqlConnection coneccion = new SqlConnection("server=SISTE200 ; database = SISTEMA ; INTEGRATED SECURITY = true"); 
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            coneccion.Open(); //abierta la base de datos 
+            SqlCommand comando = new SqlCommand("SELECT USUARIO, CONTRASENA FROM PERSONA WHERE USUARIO = @vusuario AND CONTRASENA = @vcontrasena", coneccion);
+            comando.Parameters.AddWithValue("@vusuario", textUsuario.Text);
+            comando.Parameters.AddWithValue("@vcontrasena", textContrasena.Text);
+
+            SqlDataReader lector = comando.ExecuteReader(); //lector de sql y se guarda en lector 
+
+            if (lector.Read())
+            {
+                coneccion.Close(); //cerrar consulta
+                home pantalla = new home(); //darle paso a el gome si es correcta la consulta 
+                pantalla.Show(); 
+            }
+
+
+        }
+    }
+}
